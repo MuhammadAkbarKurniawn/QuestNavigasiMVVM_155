ee617f5f9166318edb4b9fc22c395192bf192d6a
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-11-16 11:57:25 +0700

    menambahkan navcontroller dan menambahkan button back dengan popbackstack

--- app/src/main/java/com/example/activitymvvm/ui/view/TampilMahasiswaView.kt
+++ app/src/main/java/com/example/activitymvvm/ui/view/TampilMahasiswaView.kt
@@ -1,20 +1,25 @@
 package com.example.activitymvvm.ui.view
 
+import android.widget.Button
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import com.example.activitymvvm.model.Mahasiswa
 
 @Composable
 fun TampilMahasiswaView(
-    mhs:Mahasiswa
+    mhs:Mahasiswa,
+    navController: NavHostController
 ){
     Column (modifier = Modifier.fillMaxSize()){
         TampilData(
@@ -41,6 +46,11 @@ fun TampilMahasiswaView(
             judul = "Alamat",
             isinya = mhs.alamat
         )
+
+        Button(onClick = { navController.popBackStack() },
+            modifier = Modifier.padding(16.dp)) {
+            Text("Back")
+        }
     }
 }
 
